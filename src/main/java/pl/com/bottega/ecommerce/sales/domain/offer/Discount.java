c1412e85267526bc2025683c310db5f81bf96852
Seaborg7 <seaborg7@tlen.pl>
2019-04-02 22:36:26 +0200

    Poprawiona klasa Discount
    Money jest już prywatne

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Discount.java
@@ -1,59 +1,32 @@
 package pl.com.bottega.ecommerce.sales.domain.offer;
 
-import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Discount {
 
-	Money value;
+	private Money value;
+	private String discountCause;
+
 	
-	private String cause;
-
-	public BigDecimal getValue() {
-		return value;
-	}
-
-	public String getCause() {
-		return cause;
-	}
-
-	public String getTotalCostCurrency() {
-		return currency;
-	}
+	@Override
+    public int hashCode() {
+        return Objects.hash(discountCause, value);
+    }
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((currency == null) ? 0 : currency.hashCode());
-		result = prime * result + ((cause == null) ? 0 : cause.hashCode());
-		result = prime * result + ((value == null) ? 0 : value.hashCode());
-		return result;
-	}
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Discount discount = (Discount) o;
+        return Objects.equals(discountCause, discount.discountCause) &&
+                Objects.equals(value, discount.value);
+    }
+	
+	public Money getValue() {
+        return value;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Discount other = (Discount) obj;
-		if (currency == null) {
-			if (other.currency != null)
-				return false;
-		} else if (!currency.equals(other.currency))
-			return false;
-		if (cause == null) {
-			if (other.cause != null)
-				return false;
-		} else if (!cause.equals(other.cause))
-			return false;
-		if (value == null) {
-			if (other.value != null)
-				return false;
-		} else if (!value.equals(other.value))
-			return false;
-		return true;
-	}
+    public String getDiscountCause() {
+        return discountCause;
+    }
 }
